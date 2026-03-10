36add529a411d93f7963a1699eebeba3c370711b
lime <nikitagroshev11@gmail.com>
2026-03-10 18:38:37 +0300

    Step 5: click event handling

--- main.js
+++ main.js
@@ -7,12 +7,20 @@
 // console.log(title);
 // console.log(text);
 
-title.textContent = "DOM изменён через JS!";
+// title.textContent = "DOM изменён через JS!";
 
-title.style.color = "blue";
-title.style.backgroundColor = "#f0f0f0";
-title.style.padding = "10px";
+// title.style.color = "blue";
+// title.style.backgroundColor = "#f0f0f0";
+// title.style.padding = "10px";
 
-text.style.fontSize = "18px";
-text.style.fontStyle = "italic";
-text.style.color = "darkgreen";
+// text.style.fontSize = "18px";
+// text.style.fontStyle = "italic";
+// text.style.color = "darkgreen";
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    alert("Кнопка нажата!");
+    btn.style.backgroundColor = "#4CAF50";
+    btn.style.color = "white";
+});
